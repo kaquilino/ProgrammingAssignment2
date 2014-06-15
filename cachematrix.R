025e31e2a27261a7066417e9cd8c02995f4b15b9
Kim Aquilino <kim.aquilino@gmail.com>
2014-06-14 23:10:15 -0400

    Coded 'special' matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cache the inverse of a matrix to minimize performance
+## of repeated calls. 
 
-## Write a short comment describing this function
+## Create a "special" matrix which is actually a list 
+## containing functions to set the value of the matrix,
+## get the value of the matrix, set the inverse of the matrix, 
+## and get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+   i <- NULL
+   set <- function(y) {
+      x <<- y
+      i <<- NULL
+   }
+   get <- function() x
+   set_inverse <- function(inverse) i <<- inverse
+   get_inverse <- function() i
+   list(set = set, get = get,
+      set_inverse = set_inverse,
+      get_inverse = get_inverse)
 }
 
-
-## Write a short comment describing this function
+## Calculate the inverse of the "special" matrix. First
+## check if the inverse has already been calculated. If so,
+## use that value and skip recomputation. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   i <- x$get_inverse()
+   if(!is.null(i)) {
+      message("getting cached data")
+      return(i)
+   }
+   data <- x$get()
+   i <- solve(data, ...)
+   x$set_inverse(i)
+   i   
 }
